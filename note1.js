74d6b4920907db07865f24491f765a7a84753d22
username <maiaddress@address.com>
2021-03-18 23:11:56 +0900

    update : インデント修正

--- note1.js
+++ note1.js
@@ -1,31 +1,31 @@
 (function () {
-    /**
-     * What to write down.
-     * ex) const note = `sample text.`;
-     *     const note = `sample\ntext.`;
-     *     const note = `sample
-     *     text.`;
-     */
-    const note = ``;
+  /**
+   * What to write down.
+   * ex) const note = `sample text.`;
+   *     const note = `sample\ntext.`;
+   *     const note = `sample
+   *     text.`;
+   */
+  const note = ``;
 
-    /**
-     * If necessary, write the character style.
-     * ex) const blockStyle = 'border:1px solid red;';
-     */
-    const blockStyle = '';
+  /**
+   * If necessary, write the character style.
+   * ex) const blockStyle = 'border:1px solid red;';
+   */
+  const blockStyle = '';
 
-    /**
-     * If necessary, write the character style.
-     * ex) const characterStyle = 'color: red;';
-     *     const characterStyle = 'font-weight: bold;';
-     */
-    const characterStyle = '';
+  /**
+   * If necessary, write the character style.
+   * ex) const characterStyle = 'color: red;';
+   *     const characterStyle = 'font-weight: bold;';
+   */
+  const characterStyle = '';
 
-    const content = `
+  const content = `
         <div style="${blockStyle}">
           <pre style="${characterStyle}">${note}</pre>
         </div>
     `;
 
-    document.write(content);
+  document.write(content);
 })();
